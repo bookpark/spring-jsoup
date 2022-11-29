07a7904a206ba98e6fa62da8da00671c5f0fecfa
bookpark <bkbkgg@gmail.com>
2022-11-29 10:53:10 +0900

    initial commit

---
+++ src/test/java/mini/jsoup/JsoupApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.jsoup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
