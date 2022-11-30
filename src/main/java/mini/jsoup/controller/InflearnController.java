46c97e657ea73bb80f75f2d2b6326948c4c507ca
bookpark <bkbkgg@gmail.com>
2022-11-30 09:41:48 +0900

    강의 리스트 크롤링 구현

---
+++ src/main/java/mini/jsoup/controller/InflearnController.java
@@ -0,0 +1,97 @@
+package mini.jsoup.controller;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+public class InflearnController {
+
+    final String inflearnUrl = "https://www.inflearn.com/courses/it-programming?order=seq&page=";
+    private final int FIRST_PAGE_INDEX = 1;
+    private final int LAST_PAGE_INDEX = 3;
+
+
+    @GetMapping("/api/inflearn")
+    public String all() {
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            for (int i = FIRST_PAGE_INDEX; i <= LAST_PAGE_INDEX; i++) {
+                String url = inflearnUrl + i;
+                Connection conn = Jsoup.connect(url);
+                Document doc = conn.get();
+
+                Elements images = doc.getElementsByClass("swiper-lazy");
+                Elements titles = doc.select("div.card-content>div.course_title");
+                Elements prices = doc.select("div.price");
+                Elements instructors = doc.select("div.instructor");
+                Elements descriptions = doc.select("p.course_description");
+                Elements skills = doc.select("div.course_skills>span");
+                Elements urls = doc.select("a.course_card_front");
+
+
+                for (int j = 0; j < images.size(); j++) {
+                    String image = images.get(j).attr("abs:src");
+                    String title = titles.get(j).text();
+                    String price = prices.get(j).text();
+                    int realPrice = 0, salePrice = 0;
+                    if (!price.equals("무료")) {
+                        realPrice = toInt(getRealPrice(price).replace("\\₩", ""));
+                        salePrice = toInt(getSalePrice(price).replace("\\₩", ""));
+                    }
+                    String instructor = instructors.get(j).text();
+                    String description = descriptions.get(j).text();
+                    String skill = skills.get(j).text();
+
+                    String innerUrl = urls.get(j).attr("abs:href");
+
+                    Connection innerConn = Jsoup.connect(innerUrl);
+                    Document innerDoc = innerConn.get();
+
+                    Element ratings = innerDoc.selectFirst("div.dashboard-star__num");
+                    double rating = Objects.isNull(ratings) ? 0.0 : Double.parseDouble(ratings.text());
+                    rating = Math.round(rating * 10) / 10.0;
+
+                    sb.append(image).append("<br />");
+                    sb.append(title).append("<br />");
+                    sb.append(price).append("<br />");
+                    if (realPrice != salePrice) {
+                        sb.append(salePrice).append("<br />");
+                    }
+
+                    sb.append(instructor).append("<br />");
+                    sb.append(innerUrl).append("<br />");
+                    sb.append(description).append("<br />");
+                    sb.append(skill).append("<br />");
+                    sb.append(rating).append("<br />").append("<br />");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+
+    }
+
+    private String getRealPrice(String price) {
+        return price.split(" ")[0];
+    }
+
+    private String getSalePrice(String price) {
+        String[] prices = price.split(" ");
+        return prices.length == 1 ? prices[0] : prices[1];
+    }
+
+    private int toInt(String str) {
+        str = str.replaceAll("₩", "");
+        str = str.replaceAll(",", "");
+        return Integer.parseInt(str);
+    }
+}
